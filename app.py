11e4412ab8a0ef0fb1c9ea5304f8e3b5f3eca3b1
aosborne17 <AOsborne@spartaglobal.com>
2020-07-22 15:41:40 +0100

    'creating_question_page'

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, url_for, request, redirect
 # In order to create flask we must first create an instance of the flask class
 app = Flask(__name__)
 
+# creating a secret key, personal information so this should be random
+# this secret key should then be put into another file that we would import from
+app.secret_key = "secret key"
+
 """
 @app.route is a decorator that Flask provides to assign URLs.
 The decorator is telling our app that whenever the user enters the "/" domain
@@ -12,21 +16,36 @@ then run the index function.
 
 # Here we are creating the web route
 # We are sending info from the controller to the 'view' part
-@app.route('/', methods=['GET', 'POST'])
+"""
+By default, GET is the default method if nothing else is explicitly defined.
+Here we have added POST method so the user can send a post request with their login credentials.
+The code within this function checks to see if the credentials are correct, if so redirecting
+the user to the home page
+"""
+
+
+@app.route('/')
+def home():
+    return render_template('welcome.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
 # This method would allow the message to be displayed on the web route specified
-def index():
+def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'AOsborne' or request.form['password'] != 'Password':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('home'))
-    return render_template('base.html', error=error)
+            # this url_for function generates an endpoint for the method
+            return redirect(url_for('question'))
+    return render_template('login.html', error=error)
 
 
-@app.route('/home')
-def home():
-    return "WE MADE IT"
+@app.route('/question')
+def question():
+    return render_template('question.html')
+    # return "It's time..."
 
 
 if __name__ == "__main__":
